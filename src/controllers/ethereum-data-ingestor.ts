32906f4e42d5df896930a6b7e70787725c4e284e
gsofter <real.kingstar@outlook.com>
2024-03-29 14:11:48 -0500

    integrate redis for pub/sub model

--- src/controllers/ethereum-data-ingestor.ts
+++ src/controllers/ethereum-data-ingestor.ts
@@ -1,83 +1,92 @@
 import { ethers } from "ethers";
 import { PrismaClient } from "@prisma/client";
+import Redis from "ioredis";
+
+export interface IBlock {
+  number: number;
+  hash: string;
+  parentHash: string;
+  timestamp: number;
+  nonce?: string;
+  difficulty: number;
+  gasLimit: string;
+  gasUsed: string;
+  miner: string;
+  extraData: string;
+  baseFeePerGas?: null | string;
+}
+
+export interface ITransaction {
+  hash: string;
+  to?: string;
+  from: string;
+  nonce: number;
+  gas: string;
+  gasPrice?: string;
+  data: string;
+  value: string;
+  blockNumber?: number;
+  blockHash?: string;
+  timestamp?: number;
+  confirmations: number;
+}
 
 export class EthereumDataIngester {
   private provider: ethers.providers.JsonRpcProvider;
   private prisma: PrismaClient;
+  private redis: Redis;
 
-  constructor(providerUrl: string) {
+  constructor(providerUrl: string, redisUrl: string) {
     this.provider = new ethers.providers.JsonRpcProvider(providerUrl);
     this.prisma = new PrismaClient();
+    this.redis = new Redis(redisUrl);
   }
 
-  async fetchAndStoreTransactions(blockNumber: number): Promise<void> {
+  async publish(channelName: string, messageData: any): Promise<void> {
+    await this.redis.publish(channelName, JSON.stringify(messageData));
+  }
+
+  async fetchTransactions(blockNumber: number): Promise<void> {
     try {
       const block = await this.provider.getBlockWithTransactions(blockNumber);
-      console.log(`Storing transactions for block ${blockNumber}`);
+      console.log(`Fetch block for ${blockNumber}`);
       console.log({ block });
 
-      // create new block
-      await this.prisma.block.upsert({
-        where: {
-          number: block.number,
-        },
-        create: {
-          number: block.number,
-          hash: block.hash,
-          parentHash: block.parentHash,
-          nonce: block.nonce,
-          difficulty: block.difficulty,
-          gasLimit: block.gasLimit.toString(),
-          gasUsed: block.gasUsed.toString(),
-          miner: block.miner,
-          timestamp: block.timestamp,
-          extraData: block.extraData,
-        },
-        update: {
-          number: block.number,
-          hash: block.hash,
-          parentHash: block.parentHash,
-          nonce: block.nonce,
-          difficulty: block.difficulty,
-          gasLimit: block.gasLimit.toString(),
-          gasUsed: block.gasUsed.toString(),
-          miner: block.miner,
-          timestamp: block.timestamp,
-          extraData: block.extraData,
-        },
-      });
+      const { transactions, ...blockData } = block;
+      const blockMessageData: IBlock = {
+        number: block.number,
+        hash: block.hash,
+        parentHash: block.parentHash,
+        nonce: block.nonce,
+        difficulty: block.difficulty,
+        gasLimit: block.gasLimit.toString(),
+        gasUsed: block.gasUsed.toString(),
+        miner: block.miner,
+        timestamp: block.timestamp,
+        extraData: block.extraData,
+      };
+
+      await this.storeBlock(blockMessageData);
 
       for (const tx of block.transactions) {
-        await this.prisma.transaction.upsert({
-          where: {
-            hash: tx.hash,
-          },
-          create: {
-            hash: tx.hash,
-            blockNumber: blockNumber,
-            blockHash: block.hash,
-            nonce: tx.nonce,
-            from: tx.from,
-            to: tx.to,
-            value: tx.value.toString(),
-            gas: tx.gasLimit.toString(),
-            gasPrice: tx.gasPrice?.toString(),
-            input: tx.data,
-          },
-          update: {
-            blockNumber: blockNumber,
-            blockHash: block.hash,
-            nonce: tx.nonce,
-            from: tx.from,
-            to: tx.to,
-            value: tx.value.toString(),
-            gas: tx.gasLimit.toString(),
-            gasPrice: tx.gasPrice?.toString(),
-            input: tx.data,
-          },
-        });
+        const txMessageData: ITransaction = {
+          hash: tx.hash,
+          blockNumber: tx.blockNumber || 0,
+          blockHash: tx.blockHash || "",
+          nonce: tx.nonce,
+          from: tx.from,
+          to: tx.to,
+          value: tx.value.toString(),
+          gas: tx.gasLimit.toString(),
+          gasPrice: tx.gasPrice?.toString(),
+          data: tx.data,
+          confirmations: tx.confirmations,
+        };
+
+        this.publish("transaction", txMessageData);
+        console.log(`Tx for ${tx.hash} queued.`);
       }
-      console.log(`Transactions for block ${blockNumber} stored successfully.`);
+      console.log(`Transactions for block ${blockNumber} queued successfully.`);
     } catch (error) {
       console.error(
         `Error storing transactions for block ${blockNumber}:`,
@@ -86,26 +95,15 @@ export class EthereumDataIngester {
     }
   }
 
-  async fetchAndStoreInternalTransactions(blockNumber: number): Promise<void> {
+  async fetchLogs(blockNumber: number): Promise<void> {
     try {
       const logs = await this.provider.getLogs({
         fromBlock: blockNumber,
         toBlock: blockNumber,
       });
-      console.log(`Storing internal transactions for block ${blockNumber}`);
+
       for (const log of logs) {
-        await this.prisma.log.create({
-          data: {
-            transactionHash: log.transactionHash,
-            logIndex: log.logIndex,
-            blockNumber: blockNumber,
-            blockHash: log.blockHash,
-            address: log.address,
-            data: log.data,
-            topics: log.topics,
-            removed: log.removed,
-          },
-        });
+        this.publish("log", log);
       }
       console.log(
         `Internal transactions for block ${blockNumber} stored successfully.`
@@ -118,12 +116,93 @@ export class EthereumDataIngester {
     }
   }
 
-  async fetchAndStoreBlockData(blockNumber: number): Promise<void> {
-    await this.fetchAndStoreTransactions(blockNumber);
-    await this.fetchAndStoreInternalTransactions(blockNumber);
+  async storeBlock(block: IBlock) {
+    const { number, ...updates } = block;
+    // create new block or update existing one
+    await this.prisma.block.upsert({
+      where: {
+        number: block.number,
+      },
+      create: {
+        number: block.number,
+        ...updates,
+      },
+      update: {
+        ...updates,
+      },
+    });
+
+    console.log(`Block stored for ${block.number}`);
   }
 
-  async disconnect(): Promise<void> {
+  async storeTransaction(tx: ITransaction) {
+    await this.prisma.transaction.upsert({
+      where: {
+        hash: tx.hash,
+      },
+      create: {
+        hash: tx.hash,
+        blockNumber: tx.blockNumber || 0,
+        blockHash: tx.blockHash || "",
+        nonce: tx.nonce,
+        from: tx.from,
+        to: tx.to,
+        value: tx.value,
+        gas: tx.gas,
+        gasPrice: tx.gasPrice,
+        input: tx.data,
+      },
+      update: {
+        blockNumber: tx.blockNumber || 0,
+        blockHash: tx.blockHash || "",
+        nonce: tx.nonce,
+        from: tx.from,
+        to: tx.to,
+        value: tx.value,
+        gas: tx.gas,
+        gasPrice: tx.gasPrice,
+        input: tx.data,
+      },
+    });
+
+    console.log(`Tx stored for ${tx.hash}`);
+  }
+
+  async storeLog(log: ethers.providers.Log) {
+    const logFound = await this.prisma.log.findFirst({
+      where: {
+        transactionHash: log.transactionHash,
+      },
+    });
+
+    if (logFound) console.log(`Log for ${log.transactionHash} existing`);
+
+    await this.prisma.log.create({
+      data: {
+        transactionHash: log.transactionHash,
+        logIndex: log.logIndex,
+        blockNumber: log.blockNumber,
+        blockHash: log.blockHash,
+        address: log.address,
+        data: log.data,
+        topics: log.topics,
+        removed: log.removed,
+      },
+    });
+
+    console.log(`Log stored for ${log.transactionHash}`);
+  }
+
+  async startFetchingBlockData(blockNumber: number): Promise<void> {
+    await this.fetchTransactions(blockNumber);
+    await this.fetchLogs(blockNumber);
+  }
+
+  async disconnectDB(): Promise<void> {
     await this.prisma.$disconnect();
+  }
+
+  async disconnectRedis(): Promise<void> {
+    await this.redis.disconnect();
   }
 }
--- src/script.ts
+++ src/script.ts
@@ -1,18 +1,44 @@
+import { Redis } from "ioredis";
 import { EthereumDataIngester } from "./controllers/ethereum-data-ingestor";
 // Specify Ethereum provider URL and PostgreSQL database URL
-const providerUrl = "https://eth.llamarpc.com";
+const providerUrl = process.env.RPC_PROVIDER_URL || "https://eth.llamarpc.com	";
+const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
 
 // Create an instance of EthereumDataIngester class
-const dataIngester = new EthereumDataIngester(providerUrl);
+const dataIngester = new EthereumDataIngester(providerUrl, redisUrl);
 
+const sub = new Redis();
+sub.subscribe("transaction", "block", "log", (err, count) => {
+  if (err) {
+    console.error("Failed to subscribe: %s", err.message);
+  } else {
+    console.log(
+      `Subscribed successfully! This client is currently subscribed to ${count} channels.`
+    );
+  }
+});
+
+sub.on("message", (channel, message) => {
+  if (channel === "block") {
+    const block = JSON.parse(message);
+    dataIngester.storeTransaction(block);
+  }
+
+  if (channel === "transaction") {
+    const tx = JSON.parse(message);
+    dataIngester.storeTransaction(tx);
+  }
+
+  if (channel === "log") {
+    const log = JSON.parse(message);
+    dataIngester.storeLog(log);
+  }
+});
+
+// Random block number for trial purpose
 const blockNumber = 19536449;
-// Fetch and store data for the specified block
 
 dataIngester
-  .fetchAndStoreBlockData(blockNumber)
-  .then(() => console.log("Data fetched and stored successfully."))
+  .startFetchingBlockData(blockNumber)
   .catch((error) => console.error("Error fetching and storing data:", error))
-  .finally(async () => {
-    // Disconnect Prisma client after use
-    await dataIngester.disconnect();
-  });
+  .finally(async () => {});
